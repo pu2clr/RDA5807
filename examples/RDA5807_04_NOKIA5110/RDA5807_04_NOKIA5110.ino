d1056a66be7c5038edd188abbfe9a62434db9701
Ricardo Lima Caratti <pu2clr@gmail.com>
2023-04-05 16:54:55 -0300

    Examples

--- examples/RDA5807_04_NOKIA5110/RDA5807_04_NOKIA5110.ino
+++ examples/RDA5807_04_NOKIA5110/RDA5807_04_NOKIA5110.ino
@@ -139,7 +139,7 @@ void setup()
   // Start the Nokia display device
   display.begin();  
   // ATTENTION: YOU MUST VERIFY THE BEST LAVEL FOR THE CONTRAST OF YOUR DISPLAY.
-  display.setContrast(60);    // You may need adjust this value for you Nokia 5110
+  display.setContrast(70);    // You may need adjust this value for you Nokia 5110
   showSplash();
   showTemplate();
 
