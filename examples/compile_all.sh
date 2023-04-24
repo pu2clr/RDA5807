9683a6b83de67b6f72239305c046f97be76da965
Ricardo Lima Caratti <pu2clr@gmail.com>
2023-04-24 07:08:12 -0300

    Examples

--- examples/compile_all.sh
+++ examples/compile_all.sh
@@ -57,6 +57,7 @@ arduino-cli compile --fqbn ATTinyCore:avr:attinyx4:millis=enabled  ./RDA5807_03_
 echo "ESP8266 OLED"
 arduino-cli compile --fqbn esp8266:esp8266:generic  ./UNDER_CONSTRUCTION/RDA5807_06_ESP8266_OLED --output-dir ~/Downloads/hex/ESP8266/RDA5807_06_ESP8266_OLED  --warnings all
 
+# compile UNO
 echo "UNO"
 arduino-cli compile -b arduino:avr:uno  ./UNDER_CONSTRUCTION/RDA5807_07_NANO_OLED --output-dir ~/Downloads/hex/ESP8266/RDA5807_07_NANO_OLED  --warnings all
 
